bd099649209291bcdd8f949ef4cffe8dc5eab920
RamanSB <ramansb100@gmail.com>
2020-12-04 22:45:23 +0000

    Accomodates for cast characters where URL/Hyperlinks <a href="..."></a> do not exist

--- web-scraper.js
+++ web-scraper.js
@@ -10,11 +10,15 @@ to identify elements of interest.
 
 This js script is responsible for scraping an IMDB title URL and extracting the character names
 played by the cast members and storing this information in a file.
+
+Please modify the argument passed in on line 60 with the 3 URLS below or any URL or your
+liking from the IMDB title search.
 */
 
 let hyperlinkClass = "sc-link-dark.sc-type-light.playableTile__mainHeading.audibleTile__mainHeading.playableTile__heading.playableTile__audibleHeading.audibleTile__audibleHeading.sc-truncate.sc-font-light"
 let harryPotterExample = 'https://www.imdb.com/title/tt0241527/?ref_=fn_al_tt_1';
 let batmanUrl = "https://www.imdb.com/title/tt1877830/?ref_=nv_sr_srsg_0";
+let queenGambitUrl = 'https://www.imdb.com/title/tt10048342/?ref_=nv_sr_srsg_0';
 
 async function makeRequest(imdbMovieUrl){
   let characters = [];
@@ -27,20 +31,31 @@ async function makeRequest(imdbMovieUrl){
       .find('h1')
       .text();
       fs.writeFileSync("./scraped-data.txt", movieTitle+"\n"+"---Cast---\n");
-    let urlElems = $('#titleCast > table > tbody > tr > td.character');//cheerio takes the CSS selector patha as an argument
-    let elemIndex = Object.keys(urlElems).filter(x => !isNaN(x));
 
-    for(let i=0; i<elemIndex.length; i++){
-      let characterName = $(urlElems[i]).find('a').text();
+    let castTableElem = $('#titleCast > table > tbody > tr > td.character');
+    let castTableElemIdx = Object.keys(castTableElem).filter(x=>!isNaN(x));
+    console.log(castTableElemIdx);
+
+    for(let i=0; i<castTableElemIdx.length; i++){
+      let characterName;
+      if($(castTableElem[i]).find('a').attr('href')=='#'){
+        characterName = $($(castTableElem[i]).contents()['0']).text().trim()
+      }else if($(castTableElem[i]).find('a').attr('href') != null){
+        characterName = $(castTableElem[i]).children().first().text();
+      }else{
+        console.log('no hyperlinks exist');
+        characterName = $(castTableElem[i]).contents().text().trim();
+      }
       characters.push(characterName);
     }
 
-    console.log(characters.join('\n'));
-    fs.appendFileSync("./scraped-data.txt", characters.join('\n'));
+     characters = characters.filter(x => x.trim().length > 0);
+     console.log(characters.join('\n'));
+     fs.appendFileSync("./scraped-data.txt", characters.join('\n'));
   } catch (err) {
-      console.error(`We are within the catch block as an error occurred`);
+      console.error(`We are within the catch block as an error occurred` + err);
   }
 }
 
 
-makeRequest(batmanUrl);
+makeRequest(queenGambitUrl);
